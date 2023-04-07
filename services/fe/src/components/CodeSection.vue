a5404f8aee6a8b3bab6bfe1653e2cf2c54a3d348
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-07 23:49:28 +1200

    Added code block deps and basic component

--- services/fe/src/components/CodeSection.vue
+++ services/fe/src/components/CodeSection.vue
@@ -1,7 +1,46 @@
-<script>
-
-</script>
-
+<!-- CodeEditor.vue -->
 <template>
-    <!-- v-container with a v-for rendering a list of cards -->
+    <div>
+        <codemirror v-model="code" :options="editorOptions" @input="onInput" ref="editor"></codemirror>
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { Codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/python/python";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/edit/matchbrackets";
+
+export default defineComponent({
+    components: { Codemirror },
+    props: {
+        value: {
+            type: String,
+            default: "",
+        },
+    },
+    setup(props, { emit }) {
+        const code = ref(props.value);
+        const editorOptions = {
+            mode: "python",
+            theme: "material",
+            lineNumbers: true,
+            autoCloseBrackets: true,
+            matchBrackets: true,
+        };
+
+        function onInput() {
+            emit("update:modelValue", code.value);
+        }
+
+        return {
+            code,
+            editorOptions,
+            onInput,
+        };
+    },
+});
+</script>
