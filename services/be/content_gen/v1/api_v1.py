ddd2fdc781357554a1bdedbe9ff831df5aa71399
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2023-04-08 11:39:46 +1200

    fix response models

--- services/be/content_gen/v1/api_v1.py
+++ services/be/content_gen/v1/api_v1.py
@@ -7,8 +7,8 @@ from be.shared.models import CodeBlock, CodeQuestion, Question
 api_v1_router = APIRouter(prefix="/v1")
 
 
-@api_v1_router.post("/create-question", response_model=CodeQuestion)
-def create_question(user_request: GenerateQuestionRequest):
+@api_v1_router.post("/generate-question", response_model=GenerateCodeQuestionResponse)
+async def create_question(user_request: GenerateQuestionRequest):
     return GenerateCodeQuestionResponse(
         code_question=CodeQuestion(
             question=Question(
@@ -28,16 +28,16 @@ def create_question(user_request: GenerateQuestionRequest):
     )
 
 
-@api_v1_router.post("/hint")
-def get_hint(hint_request: GenerateCodeHintRequest) -> GenerateTextResponse:
+@api_v1_router.post("/hint", response_model=GenerateTextResponse)
+async def get_hint(hint_request: GenerateCodeHintRequest) -> GenerateTextResponse:
     return GenerateTextResponse(
         text="You can use the return keyword to return a value",
         tokens_used=1234,
     )
 
 
-@api_v1_router.post("/give-up")
-def give_up(hint_request: GenerateCodeHintRequest) -> GenerateTextResponse:
+@api_v1_router.post("/give-up", response_model=GenerateTextResponse)
+async def give_up(hint_request: GenerateCodeHintRequest) -> GenerateTextResponse:
     return GenerateTextResponse(
         text="Wow you suck, it was pretty simple my dude",
         tokens_used=1234,
