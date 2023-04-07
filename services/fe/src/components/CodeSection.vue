fc6e1aa594f7acc918ef638a2f38f38278b82fc0
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-08 09:29:22 +1200

    removed unneeded comments

--- services/fe/src/components/CodeSection.vue
+++ services/fe/src/components/CodeSection.vue
@@ -8,12 +8,7 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { Codemirror } from "vue-codemirror";
-// import "codemirror/lib/codemirror.css";
-// import "codemirror/theme/material.css";
-// import "codemirror/mode/python/python";
 import { python } from '@codemirror/lang-python'
-// import "codemirror/addon/edit/closebrackets";
-// import "codemirror/addon/edit/matchbrackets";
 
 export default defineComponent({
     components: { Codemirror },
@@ -28,6 +23,7 @@ export default defineComponent({
         const extensions = [python()]
         const editorOptions = {
             mode: "python",
+            // are additional plugins/addons needed here?
             theme: "material",
             lineNumbers: true,
             autoCloseBrackets: true,
