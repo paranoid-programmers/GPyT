f804e7fb3f9961b32c60fefdb22b252c3ff1022c
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-07 19:14:27 +1200

    Using more standard options api

---
+++ services/fe/src/components/CodeSection.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+    <!-- v-container with a v-for rendering a list of cards -->
+</template>
--- services/fe/src/components/TutorialFeed.vue
+++ services/fe/src/components/TutorialFeed.vue
@@ -21,26 +21,25 @@ export default defineComponent({
         TutorialCard,
         UserInputForm
     },
-    setup() {
-        const tutorials = ref<Tutorial[]>([]);
-        const generateTutorial = async (topic: string) => {
+    data(): { tutorials: Tutorial[] } {
+        return {
+            tutorials: []
+        }
+    },
+    methods: {
+        generateTutorial(topic: string) {
             // Call your backend API to generate the tutorial content.
             // For example:
             // const response = await axios.post('/api/generate-tutorial', { topic });
             // tutorials.value.push(response.data);
 
             console.log("hello", topic)
-            tutorials.value.push({
+            this.tutorials.push({
                 id: Math.random(),
                 title: topic,
                 content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eget nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eget nisl.'
             });
-        };
-
-        return {
-            tutorials,
-            generateTutorial
-        };
+        }
     }
 });
 </script>
