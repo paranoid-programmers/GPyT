a4e84f391527ec4607961c3277f15ff5d4357ae3
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-08 00:16:38 +1200

    Got python syntax highlighting working

--- services/fe/src/components/CodeSection.vue
+++ services/fe/src/components/CodeSection.vue
@@ -1,18 +1,19 @@
 <!-- CodeEditor.vue -->
 <template>
     <div>
-        <codemirror v-model="code" :options="editorOptions" @input="onInput" ref="editor"></codemirror>
+        <codemirror v-model="code" :options="editorOptions" @input="onInput" ref="editor" :extensions="extensions" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { Codemirror } from "vue-codemirror";
-import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material.css";
-import "codemirror/mode/python/python";
-import "codemirror/addon/edit/closebrackets";
-import "codemirror/addon/edit/matchbrackets";
+// import "codemirror/lib/codemirror.css";
+// import "codemirror/theme/material.css";
+// import "codemirror/mode/python/python";
+import { python } from '@codemirror/lang-python'
+// import "codemirror/addon/edit/closebrackets";
+// import "codemirror/addon/edit/matchbrackets";
 
 export default defineComponent({
     components: { Codemirror },
@@ -24,6 +25,7 @@ export default defineComponent({
     },
     setup(props, { emit }) {
         const code = ref(props.value);
+        const extensions = [python()]
         const editorOptions = {
             mode: "python",
             theme: "material",
@@ -38,6 +40,7 @@ export default defineComponent({
 
         return {
             code,
+            extensions,
             editorOptions,
             onInput,
         };
--- services/fe/src/components/TutorialQuestion.vue
+++ services/fe/src/components/TutorialQuestion.vue
@@ -2,20 +2,25 @@
     <v-card>
         <v-card-title>{{ question.title }}</v-card-title>
         <v-card-text>{{ question.description }}</v-card-text>
+        <code-section :value="question.skeleton_code.code" />
     </v-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { CodeQuestion } from '@/types';
+import CodeSection from './CodeSection.vue';
 
 export default defineComponent({
-    name: 'TutorialQuestion',
+    name: "TutorialQuestion",
     props: {
         question: {
             type: Object as () => CodeQuestion,
             required: true
         }
+    },
+    components: {
+        CodeSection
     }
 });
 </script>
