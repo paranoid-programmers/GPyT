7c356fe82da1fdfbf2a2f1a528f93717954fd460
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-11 13:35:56 +1200

    Moved dark mode to nav drawer

--- src/fe/src/layouts/default/NavigationDrawer.vue
+++ src/fe/src/layouts/default/NavigationDrawer.vue
@@ -1,16 +1,25 @@
 <template>
-  <v-navigation-drawer class="bg-deep-purple" theme="dark" permanent app>
-    <v-list color="transparent">
+  <v-navigation-drawer permanent app>
+    <v-list nav density="compact">
       <v-list-item
         prepend-icon="mdi-view-dashboard"
         title="Dashboard"
-      ></v-list-item>
-      <v-list-item prepend-icon="mdi-account-box" title="Account"></v-list-item>
+        value="dasboard"
+      />
+      <v-list-item
+        prepend-icon="mdi-account-box"
+        title="Account"
+        value="home"
+      />
       <v-list-item prepend-icon="mdi-gavel" title="Admin"></v-list-item>
     </v-list>
-
     <template v-slot:append>
+      <!-- centered div -->
       <div class="pa-2">
+        <v-switch
+          :label="$vuetify.theme.current.dark ? 'Light mode' : 'Dark mode'"
+          @change="toggleTheme"
+        ></v-switch>
         <v-btn block> Logout </v-btn>
       </div>
     </template>
@@ -19,12 +28,19 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useTheme } from 'vuetify'
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const theme = useTheme()
+
+    return {
+      theme,
+      toggleTheme: () =>
+        (theme.global.name.value = theme.global.current.value.dark
+          ? 'light'
+          : 'dark'),
+    }
+  },
+})
 </script>
-
-<style scoped>
-.v-main {
-  height: 400px;
-}
-</style>
