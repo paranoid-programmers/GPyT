ab4c1da981dd5961b46d508c2ff1c9ed06aeccc8
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-24 11:40:09 +1200

    Ran black

--- src/be/api/hahabusiness/code_tutorial_service.py
+++ src/be/api/hahabusiness/code_tutorial_service.py
@@ -2,8 +2,14 @@ import uuid
 from be.api.clients.content_gen_client import ContentGenClient, get_content_gen_client
 from be.api.clients.supabase_client import SupabaseWrapper, get_supabase_client
 from be.api.internal.models import CodeTutorial, UniqueCodeQuestion, ReportedQuestion
-from be.api.v1.models.response_models import NewCodeTutorialResponse, PositiveAffirmationResponse, HintResponse, \
-    GiveUpResponse, MoreQuestionsResponse, ReportQuestionResponse
+from be.api.v1.models.response_models import (
+    NewCodeTutorialResponse,
+    PositiveAffirmationResponse,
+    HintResponse,
+    GiveUpResponse,
+    MoreQuestionsResponse,
+    ReportQuestionResponse,
+)
 from be.shared.models import TutorialContext, CodeBlock
 from fastapi import HTTPException
 
@@ -13,21 +19,31 @@ def get_code_tutorial_service():
 
 
 class CodeTutorialService:
-    def __init__(self, content_client: ContentGenClient, supabase_client: SupabaseWrapper):
+    def __init__(
+        self, content_client: ContentGenClient, supabase_client: SupabaseWrapper
+    ):
         self.content_client = content_client
         self.supabase_client = supabase_client
 
-    async def create_new_tutorial(self, tutorial_context: TutorialContext, concept: str) -> NewCodeTutorialResponse:
+    async def create_new_tutorial(
+        self, tutorial_context: TutorialContext, concept: str
+    ) -> NewCodeTutorialResponse:
         # ask content client for a single question
-        question_response = await self.content_client.generate_question(tutorial_context, concept)
+        question_response = await self.content_client.generate_question(
+            tutorial_context, concept
+        )
         question_response.code_question.concept = concept
 
         # create a code tutorial
         tutorial_uuid = uuid.uuid4()
-        tutorial = CodeTutorial(uuid=tutorial_uuid, questions=[], context=tutorial_context)
+        tutorial = CodeTutorial(
+            uuid=tutorial_uuid, questions=[], context=tutorial_context
+        )
 
         # unique-ify the question and add it to the tutorial
-        unique_code_question = UniqueCodeQuestion(uuid=uuid.uuid4(), question=question_response.code_question)
+        unique_code_question = UniqueCodeQuestion(
+            uuid=uuid.uuid4(), question=question_response.code_question
+        )
         tutorial.questions.append(unique_code_question)
 
         # persist the question in supabase
@@ -38,7 +54,12 @@ class CodeTutorialService:
 
         return NewCodeTutorialResponse(tutorial=tutorial)
 
-    async def get_hint(self, tutorial_uuid: uuid, question_uuid: uuid, user_code: CodeBlock) -> HintResponse:
+    async def get_hint(
+        self,
+        tutorial_uuid: uuid,
+        question_uuid: uuid,
+        user_code: CodeBlock,
+    ) -> HintResponse:
         # fetch the tutorial from supabase
         tutorial = await self.supabase_client.get_tutorial(tutorial_uuid)
         if tutorial is None:
@@ -50,10 +71,14 @@ class CodeTutorialService:
             raise HTTPException(status_code=404, detail="Question not found")
 
         # ask content client for a hint
-        hint = await self.content_client.generate_hint(question.question, tutorial.context, user_code)
+        hint = await self.content_client.generate_hint(
+            question.question, tutorial.context, user_code
+        )
         return HintResponse(hint_text=hint.text)
 
-    async def give_up(self, tutorial_uuid: uuid, question_uuid: uuid, user_code: CodeBlock) -> GiveUpResponse:
+    async def give_up(
+        self, tutorial_uuid: uuid, question_uuid: uuid, user_code: CodeBlock
+    ) -> GiveUpResponse:
         # fetch the tutorial from supabase
         tutorial = await self.supabase_client.get_tutorial(tutorial_uuid)
         if tutorial is None:
@@ -65,8 +90,17 @@ class CodeTutorialService:
             raise HTTPException(status_code=404, detail="Question not found")
 
         # ask content client for a hint
-        give_up_message = await self.content_client.generate_give_up(question.question, tutorial.context, user_code, question.question.solution_code)
-        return GiveUpResponse(explanation=give_up_message.text, example_solution=None, additional_info=None)
+        give_up_message = await self.content_client.generate_give_up(
+            question.question,
+            tutorial.context,
+            user_code,
+            question.question.solution_code,
+        )
+        return GiveUpResponse(
+            explanation=give_up_message.text,
+            example_solution=None,
+            additional_info=None,
+        )
 
     async def more_questions(self, tutorial_uuid) -> MoreQuestionsResponse:
         # fetch the tutorial from supabase
@@ -75,10 +109,14 @@ class CodeTutorialService:
             raise HTTPException(status_code=404, detail="Tutorial not found")
 
         # ask content client for a single question
-        question_response = await self.content_client.generate_question(tutorial.context, tutorial.questions[0].question.concept)
+        question_response = await self.content_client.generate_question(
+            tutorial.context, tutorial.questions[0].question.concept
+        )
 
         # unique-ify the question and add it to the tutorial
-        unique_code_question = UniqueCodeQuestion(uuid=uuid.uuid4(), question=question_response.code_question)
+        unique_code_question = UniqueCodeQuestion(
+            uuid=uuid.uuid4(), question=question_response.code_question
+        )
         tutorial.questions.append(unique_code_question)
 
         # persist the question in supabase
@@ -89,18 +127,29 @@ class CodeTutorialService:
 
         return MoreQuestionsResponse(questions=[unique_code_question])
 
-    async def get_affirmation(self, tutorial_uuid: uuid, attempts_taken: int) -> PositiveAffirmationResponse:
+    async def get_affirmation(
+        self, tutorial_uuid: uuid, attempts_taken: int
+    ) -> PositiveAffirmationResponse:
         # fetch the tutorial from supabase
         tutorial = await self.supabase_client.get_tutorial(tutorial_uuid)
         if tutorial is None:
             raise HTTPException(status_code=404, detail="Tutorial not found")
 
         # ask content client for a single question
-        affirmation_response = await self.content_client.generate_affirmation(tutorial.context, attempts_taken)
+        affirmation_response = await self.content_client.generate_affirmation(
+            tutorial.context, attempts_taken
+        )
 
         return PositiveAffirmationResponse(happy_text=affirmation_response.text)
 
-    async def report_question(self, tutorial_uuid: uuid, question_uuid: uuid, category: str, details: str, should_regenerate: bool) -> ReportQuestionResponse:
+    async def report_question(
+        self,
+        tutorial_uuid: uuid,
+        question_uuid: uuid,
+        category: str,
+        details: str,
+        should_regenerate: bool,
+    ) -> ReportQuestionResponse:
         # fetch the tutorial from supabase
         tutorial = await self.supabase_client.get_tutorial(tutorial_uuid)
         if tutorial is None:
@@ -112,7 +161,13 @@ class CodeTutorialService:
             raise HTTPException(status_code=404, detail="Question not found")
 
         # persist the reported question in supabase
-        reported_question = ReportedQuestion(uuid=uuid.uuid4(), reported_question=bad_question, category=category, details=details, was_regenerated=should_regenerate)
+        reported_question = ReportedQuestion(
+            uuid=uuid.uuid4(),
+            reported_question=bad_question,
+            category=category,
+            details=details,
+            was_regenerated=should_regenerate,
+        )
         await self.supabase_client.insert_reported_question(reported_question)
 
         # flag the question as reported and persist it in supabase
@@ -123,10 +178,14 @@ class CodeTutorialService:
         new_question = None
         if should_regenerate:
             # ask content client for a single question
-            new_question_response = await self.content_client.generate_question(tutorial.context, tutorial.questions[0].question.concept)
+            new_question_response = await self.content_client.generate_question(
+                tutorial.context, tutorial.questions[0].question.concept
+            )
 
             # unique-ify the question and add it to the tutorial and to the response
-            new_question = UniqueCodeQuestion(uuid=uuid.uuid4(), question=new_question_response.code_question)
+            new_question = UniqueCodeQuestion(
+                uuid=uuid.uuid4(), question=new_question_response.code_question
+            )
             tutorial.questions.append(new_question)
 
             # persist the question in supabase
